a8c69a8fcd4cef64bb2514bb7ccb33b3f84f4293
n-adilla <adillanurdin@gmail.com>
2023-03-06 17:43:21 +0800

    test

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/adilla.nurdin/Documents/SKMagic/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/adilla.nurdin/Documents/SKMagic/flutter-projects/test2/group_chatapp_flutter_firebase"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/adilla.nurdin/Documents/SKMagic/flutter-projects/test2/group_chatapp_flutter_firebase/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/adilla.nurdin/Documents/SKMagic/flutter-projects/test2/group_chatapp_flutter_firebase/.dart_tool/package_config.json"
